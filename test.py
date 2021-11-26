b5f61937f3755680a3467d2f77c112d7b7055cbc
a-dempsey <amydempsey0@gmail.com>
2021-11-26 19:55:25 +0000

    Test check_valid_moves() in King class

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ class chessTest(unittest.TestCase):
     def test_king(self):
         king = King("black", "board", (0, 5))
         assert (1, 5) in King.check_valid_moves(king)
+        assert (8, 4) not in King.check_valid_moves(king)
         pass
 
 
